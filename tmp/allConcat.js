d6711a9f6814a44d596cb7fb3eb5d8cccd99ad9c
Chris Knight Johnson and Holly Wilkalis <chris@>
2018-01-10 15:46:22 -0800

    Add user input form for search

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,16 +1,20 @@
 import { apiCall } from './../js/bikeHunter.js';
+
 function addToPage(bikeInfo){
   console.log(bikeInfo);
   for (var i = 0; i < bikeInfo.bikes.length; i++) {
     console.log(i);
     var convertDate = moment.unix(bikeInfo.bikes[i].date_stolen).calendar(); // or end in .format("MM-DD-YYYY HH:mm")
     console.log(convertDate);
-    // $('div').append(`<p class="bike-item">${bikeInfo.bikes[i].title}<br>Serial Number: ${bikeInfo.bikes[i].serial}<br>Stolen From: ${bikeInfo.bikes[i].stolen_location}<br>Date Stolen: ${bikeInfo.bikes[i].date_stolen}</p>`);
-    $('div').append(`<p class="bike-item">${bikeInfo.bikes[i].title}<br>Serial Number: ${bikeInfo.bikes[i].serial}<br>Stolen From: ${bikeInfo.bikes[i].stolen_location}<br>Date Stolen: ${convertDate}</p>`);
+    $('div').append(`<p class="bike-item">Bike Description: ${bikeInfo.bikes[i].title}<br>Serial Number: ${bikeInfo.bikes[i].serial}<br>Stolen From: ${bikeInfo.bikes[i].stolen_location}<br>Date Stolen: ${convertDate}</p>`);
   }
 }
+
 $(document).ready(function(){
-  let inputZip = `97204`;
-  let inputDistance = `5`;
-  apiCall(inputZip, inputDistance, addToPage);
+  $("form#bikeSearch").submit(function(event) {
+    event.preventDefault();
+    let inputZip = $("#location").val();
+    let inputDistance = $("#distance").val();
+    apiCall(inputZip, inputDistance, addToPage);
+  });
 });
