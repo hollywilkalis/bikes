e41e1ffd5767f01aaa6198fafd8bdf2c7413f58b
Chris K Johnson and Holly Wilkalis <chris+holly@>
2018-01-11 13:49:06 -0800

    Add scripts for medical article API

--- js/bikeHunter.js
+++ js/bikeHunter.js
@@ -1,3 +1,28 @@
+export function apiCallAjax(searchTerm, perPage, addToPage, apiKey){
+  $.ajax({
+    url: `https://core.ac.uk:443/api-v2/articles/search/${searchTerm}?page=1&pageSize=${perPage}&metadata=true&fulltext=false&citations=false&similar=false&duplicate=false&urls=true&faithfulMetadata=false&apiKey=${apiKey}`,
+    type: 'GET',
+    data: {
+      format: 'json'
+    },
+    success: function(response) {
+      console.log(`Success`);
+    },
+    complete: function(response){
+      let articleInfo = JSON.parse(response.responseText);
+      addToPage(articleInfo);
+      console.log(`complete function`);
+    },
+    error: function() {
+      console.error(`API Error`);
+      // console.error(`inputted zip:${inputZip}`);
+      // console.error(`inputted dist:${inputDistance}`);
+    }
+  }).done(function(){
+    console.log(`done function`);
+  });
+}
+//AJAX API CALL FOR BIKE PROJECT
 // export function apiCallAjax(inputZip, inputDistance, addToPage){
 //   $.ajax({
 //     url: `https://bikeindex.org:443/api/v3/search?location=${inputZip}&distance=${inputDistance}&stolenness=proximity`,
@@ -22,23 +47,24 @@
 //   });
 // }
 
-export function apiCallPromise(inputZip, inputDistance, addToPage) {
-  let promise = new Promise(function(resolve, reject){
-    let request = new XMLHttpRequest();
-    let url = `https://bikeindex.org:443/api/v3/search?location=${inputZip}&distance=${inputDistance}&stolenness=proximity`;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
-  });
-
-  promise.then(function(response){
-    let bikeInfo = JSON.parse(response);
-    addToPage(bikeInfo);
-  });
-}
+//PROMISE API CALL FOR BIKE PROJECT
+// export function apiCallPromise(inputZip, inputDistance, addToPage) {
+//   let promise = new Promise(function(resolve, reject){
+//     let request = new XMLHttpRequest();
+//     let url = `https://bikeindex.org:443/api/v3/search?location=${inputZip}&distance=${inputDistance}&stolenness=proximity`;
+//     request.onload = function() {
+//       if (this.status === 200) {
+//         resolve(request.response);
+//       } else {
+//         reject(Error(request.statusText));
+//       }
+//     }
+//     request.open("GET", url, true);
+//     request.send();
+//   });
+//
+//   promise.then(function(response){
+//     let bikeInfo = JSON.parse(response);
+//     addToPage(bikeInfo);
+//   });
+// }
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,21 +1,38 @@
-import { apiCallPromise } from './../js/bikeHunter.js';
-// import { apiCallAjax } from './../js/bikeHunter.js';
+// import { apiCallPromise } from './../js/bikeHunter.js';
+import { apiCallAjax } from './../js/bikeHunter.js';
+import { apiKey } from './../.env';
+// var apiKey = require('./../.env').apiKey;
+//
+// function addToPage(bikeInfo){
+//   console.log(bikeInfo);
+//   for (var i = 0; i < bikeInfo.bikes.length; i++) {
+//     console.log(i);
+//     var convertDate = moment.unix(bikeInfo.bikes[i].date_stolen).calendar(); // or end in .format("MM-DD-YYYY HH:mm")
+//     console.log(convertDate);
+//     $('div').append(`<p class="bike-item">Bike Description: ${bikeInfo.bikes[i].title}<br>Serial Number: ${bikeInfo.bikes[i].serial}<br>Stolen From: ${bikeInfo.bikes[i].stolen_location}<br>Date Stolen: ${convertDate}</p>`);
+//   }
+// }
 
-function addToPage(bikeInfo){
-  console.log(bikeInfo);
-  for (var i = 0; i < bikeInfo.bikes.length; i++) {
-    console.log(i);
-    var convertDate = moment.unix(bikeInfo.bikes[i].date_stolen).calendar(); // or end in .format("MM-DD-YYYY HH:mm")
-    console.log(convertDate);
-    $('div').append(`<p class="bike-item">Bike Description: ${bikeInfo.bikes[i].title}<br>Serial Number: ${bikeInfo.bikes[i].serial}<br>Stolen From: ${bikeInfo.bikes[i].stolen_location}<br>Date Stolen: ${convertDate}</p>`);
-  }
+function addToPage(articleInfo){
+  console.log(articleInfo);
+  // for (var i = 0; i < bikeInfo.bikes.length; i++) {
+  //   console.log(i);
+  //   var convertDate = moment.unix(bikeInfo.bikes[i].date_stolen).calendar(); // or end in .format("MM-DD-YYYY HH:mm")
+  //   console.log(convertDate);
+  //   $('div').append(`<p class="bike-item">Bike Description: ${bikeInfo.bikes[i].title}<br>Serial Number: ${bikeInfo.bikes[i].serial}<br>Stolen From: ${bikeInfo.bikes[i].stolen_location}<br>Date Stolen: ${convertDate}</p>`);
+  // }
 }
-
 $(document).ready(function(){
-  $("form#bikeSearch").submit(function(event) {
-    event.preventDefault();
-    let inputZip = $("#location").val();
-    let inputDistance = $("#distance").val();
-    apiCallPromise(inputZip, inputDistance, addToPage);
-  });
+  // event.preventDefault();
+  let searchTerm = "vaccines";
+  let perPage = "10";
+  apiCallAjax(searchTerm, perPage, addToPage, apiKey);
 });
+// $(document).ready(function(){
+//   $("form#bikeSearch").submit(function(event) {
+//     event.preventDefault();
+//     let inputZip = $("#location").val();
+//     let inputDistance = $("#distance").val();
+//     apiCallPromise(inputZip, inputDistance, addToPage);
+//   });
+// });
